6deac74874d2cf6c38c544480c076fcd4fa96384
khandaker mushfiqur rahman <1505086.kmr@ugrad.cse.buet.ac.bd>
2020-06-20 11:32:59 +0600

    cf 1367A.cpp

--- cf_1367A.cpp
+++ cf_1367A.cpp
@@ -1,1 +1,32 @@
-ECHO is on.
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string a,b;
+    int cases;
+    cin>>cases;
+    while(cases--)
+    {
+        cin>>a;
+        if(a.size()==1 || a.size()==2)
+        {
+            cout<<a<<endl;
+        }
+        else
+        {
+
+            cout<<a[0];
+            for(int i=1; i<a.size()-1; i=i+2)
+            {
+                cout<<a[i];
+            }
+            cout<<a[a.size()-1]<<endl;
+        }
+    }
+
+
+
+    return 0;
+}
